01309334dadd06b5aa3abf711dbc8b7c17b83e55
RyanJohnsonDEV <119016031+RyanJohnsonDEV@users.noreply.github.com>
2022-12-05 12:46:56 -0700

    v0.04

---
+++ email.php
@@ -0,0 +1,44 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "chilifromchile@gmail.com";
+
+mail($recipient, "Contact Form Submission", "Phone Number: " . $phone . "\r\n" . "Message: " . $message, $mailheader) or die("Error!");
+
+?>
+
+<style type="text/css">.success{
+    visibility: visible;
+    -moz-animation: cssAnimation 0s ease-in 5s forwards;
+    /* Firefox */
+    -webkit-animation: cssAnimation 0s ease-in 5s forwards;
+    /* Safari and Chrome */
+    -o-animation: cssAnimation 0s ease-in 5s forwards;
+    /* Opera */
+    animation: cssAnimation 0s ease-in 5s forwards;
+    -webkit-animation-fill-mode: forwards;
+    animation-fill-mode: forwards;
+}
+@keyframes cssAnimation {
+    to {
+        width:0;
+        height:0;
+        overflow:hidden;
+    }
+}
+@-webkit-keyframes cssAnimation {
+    to {
+        width:0;
+        height:0;
+        visibility:hidden;
+    }
+}</style>
+
+<?php
+?>
